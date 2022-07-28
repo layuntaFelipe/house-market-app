4ee27978f60c9b6c42145810ac5119d005f82058
layuntaFelipe <layunta.felipe@gmail.com>
2022-07-28 05:14:35 -0300

    basic-project-done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -82,6 +82,10 @@ const Profile = () => {
     }
   }
 
+  const onEdit = (listingId) => {
+    navigate(`/edit-listing/${listingId}`)
+  }
+
   return <div className='profile'>
     <header className="profileHeader">
       <p className="pageHeader">My Profile</p>
@@ -117,7 +121,7 @@ const Profile = () => {
           <p className="listingText">Your Listings</p>
           <ul className="listingsList">
             {listings.map((listing) => (
-              <ListingItem key={listing.id} listing={listing.data} id={listing.id} onDelete={() => onDelete(listing.id)} />
+              <ListingItem key={listing.id} listing={listing.data} id={listing.id} onEdit={() => onEdit(listing.id)} onDelete={() => onDelete(listing.id)} />
             ))}
           </ul>
         </>
