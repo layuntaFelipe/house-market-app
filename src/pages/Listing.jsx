24d49aba7ce2dc54300c7d317747b1fb3518095a
layuntaFelipe <layunta.felipe@gmail.com>
2022-07-28 01:28:14 -0300

    adding-data-fixing-map

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -97,6 +97,13 @@ const Listing = () => {
 
         <p className="listingLocationTitle">Location</p>
         <div className="leafletContainer">
+          <MapContainer style={{height: '100%', width: '100%'}} center={[listing.geolocation.lat, listing.geolocation.lng]} zoom={13} scrollWheelZoom={false}>
+            <TileLayer attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+
+            <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
         </div>
 
         {auth.currentUser?.uid !== listing.userRef && (
